72e44b3ab352cbdaaca86c9ac79c6035a5636241
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2017-09-21 15:16:26 -0400

    modulo lottery

---
+++ Modulo/contracts/Modulo.sol
@@ -0,0 +1,72 @@
+pragma solidity ^0.4.0;
+
+contract Modulo{
+    
+    mapping (address => bytes32) tickets;
+    
+    uint public price;
+    
+    uint [] public nums;
+    address [] public players;
+    
+    
+    uint playDuration;
+    uint startTime;
+    
+    uint revealDuration;
+    
+    // contract constructor.
+    function Modulo(uint _duration, uint _revealDuration, bytes32 _nHashed) payable{
+        playDuration = _duration;
+        revealDuration = _revealDuration;
+        startTime = now;
+        price = msg.value;
+        
+       tickets[msg.sender] = _nHashed;
+        
+    
+    }
+    
+    function reveal(uint num){
+        require (startTime + playDuration >= now);
+        require(now <= startTime + playDuration + revealDuration);
+        
+        
+	if (tickets[msg.sender] == keccak256(num)){
+            nums.push(num);    
+            players.push(msg.sender);
+        
+	}
+    
+    }
+
+    // take a guess and a payment
+    
+    function submit(bytes32 nHashed) payable{
+        
+        require (now <= (startTime + playDuration));
+
+        require(msg.value == price);
+        
+        tickets[msg.sender] = nHashed;
+        
+    
+    }
+    
+    function getWinner(){
+        require (now >= startTime + playDuration + revealDuration);
+        
+        uint nSum;
+        uint N = nums.length;
+        
+	for (uint i = 0; i < N; i++){
+            nSum += nums[i];
+        
+	}
+        players[nSum % N].transfer(price * N);
+        
+    
+    }
+    
+
+}
